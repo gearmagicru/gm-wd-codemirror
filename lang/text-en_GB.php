2b1df50d5c399a5746eea3d8bf2a4f5fe2d4974d
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 19:43:30 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'CodeMirror',
+    '{description}' => 'Extensible Code Editor',
+
+    // Settings
+    '{settings.title}' => 'Настройка виджета "CodeMirror"',
+    // Settings: поля
+    'Theme' => 'Theme',
+    'Active line' => 'Active line',
+    'Fold code' => 'Fold code',
+    'Line numbers' => 'Line numbers',
+    'Addons' => 'Addons',
+    'Match brackets' => 'Match brackets',
+    'XML fold' => 'XML fold',
+    'Markdown fold' => 'Markdown fold',
+    'Indent fold' => 'Indent fold',
+    'Comment fold' => 'Comment fold',
+    'Brace fold' => 'Brace fold',
+    'Fold gutter' => 'Fold gutter',
+    'Line wrapping' => 'Line wrapping',
+    'Modes' => 'Modes',
+    'Editor CodeMirror' => 'Editor CodeMirror',
+    'version' => 'version',
+    'site' => 'site',
+    'for the editor to work correctly, it is necessary that the modes are connected' 
+        => 'for the editor to work correctly, it is necessary that the modes are connected: "HTML", "XML", "C, C++,..."',
+    // Settings: сообщения (ошибки)
+    'Modes must be selected' => 'Modes must be selected: "HTML", "XML", "C, C++,..."',
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Widget settings "CodeMirror" successfully changed.'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'CodeMirror',
+    '{description}' => 'Расширяемый редактор кода',
+
+    // Settings
+    '{settings.title}' => 'Настройка виджета "CodeMirror"',
+    // Settings: поля
+    'Theme' => 'Тема',
+    'Active line' => 'Подсвечивать активную строку',
+    'Fold code' => 'Сворачивать код',
+    'Line numbers' => 'Отображать номер строки',
+    'Addons' => 'Дополнения',
+    'Match brackets' => 'Подсвечивать скобки',
+    'XML fold' => 'Сворачивать XML теги',
+    'Markdown fold' => 'Сворачивать Markdown код',
+    'Indent fold' => 'Сворачивать отступы',
+    'Comment fold' => 'Сворачивать комментарии',
+    'Brace fold' => 'Сворачивать скобки',
+    'Fold gutter' => 'Сворачивать отступы',
+    'Line wrapping' => 'Перенос длинных строк',
+    'Modes' => 'Режимы работы редактора',
+    'Editor CodeMirror' => 'Редактор CodeMirror',
+    'version' => 'версия',
+    'site' => 'сайт',
+    'for the editor to work correctly, it is necessary that the modes are connected' 
+        => 'для корректной работы редактора, необходимо чтобы были подключены режимы: "HTML", "XML", "C, C++,..."',
+    // Settings: сообщения (ошибки)
+    'Modes must be selected' => 'Необходимо выбрать режимы: "HTML", "XML", "C, C++,..."',
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Настройки виджета "CodeMirror" успешно сохранены.'
+];
