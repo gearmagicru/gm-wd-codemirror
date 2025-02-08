5dd034abfc9f37cc26781a94e93f742cefe169de
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 19:42:19 +0300

    Файл конфигурации настроек виджета

---
+++ config/.settings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Файл конфигурации настроек виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'theme'           => 'none', // тема
+    'styleActiveLine' => false, // подсветка строки
+    'foldGutter'      => false, // сворачивать код
+    'lineWrapping'    => false, // перенос длинных строк
+    'lineNumbers'     => false, // номер строки слева
+    'matchBrackets'   => false, // подсветка скобок
+    'xmlFold'         => false, // сворачивать xml теги
+    'markdownFold'    => false, // сворачивать markdown
+    'inddentFold'     => false, // сворачивать отступв
+    'commentFold'     => false, // сворачивать комментарии
+    'braceFold'       => false, // сворачивать скобки
+    'foldCode'        => false, // сворачивать код
+    'modes'           => 'htmlmixed,xml,clike,php,css,javascript'
+];
